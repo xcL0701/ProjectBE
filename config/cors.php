a6f513f66d2bdce498f9c4402fede7dd12d82faf
xcL <vincentwilliem0511@gmail.com>
2025-04-22 22:29:32 +0700

    commit

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
     return [
         'paths' => ['api/*', 'sanctum/csrf-cookie'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['https://ptcsi-app.vercel.app'],
+        'allowed_origins' => ['https://ptcsi.vercel.app'],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
         'max_age' => 0,
