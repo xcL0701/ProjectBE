15fa324c9fd39eb28ff7876c39b79e79c442104f
xcL <vincentwilliem0511@gmail.com>
2025-05-11 20:37:23 +0700

    commit

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,6 @@
 <?php
     return [
-        'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*', 'models/*'],
+        'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*', 'storage/models/*'],
         'allowed_methods' => ['*'],
         'allowed_origins' => ['https://ptcsi.vercel.app', '*'],
         'allowed_headers' => ['*'],
