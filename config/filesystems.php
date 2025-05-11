785b448b81e98b4ad5d78e4141be68107151ae3b
xcL <vincentwilliem0511@gmail.com>
2025-05-11 16:22:31 +0700

    commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => '/app/storage/app/public',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
