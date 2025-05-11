21aa5993c717d89627859c38b18f93180364ddcf
xcL <vincentwilliem0511@gmail.com>
2025-05-11 20:45:16 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/storage/models/{filename}', function ($filename) {
     $file = file_get_contents($path);
     return response($file, 200)
         ->header('Content-Type', mime_content_type($path))
-        ->header('Access-Control-Allow-Origin', '*') // Sementara
+        ->header('Access-Control-Allow-Origin', 'https://ptcsi.vercel.app')
         ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
 });
